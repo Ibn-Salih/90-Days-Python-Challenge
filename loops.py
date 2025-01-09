70c3c0c69b8ce6cf2b8d96134762b4edcad8b456
Ibn-Salih <abdulrahmansuyuti0@gmail.com>
2025-01-09 21:17:16 +0000

    Day 4

---
+++ loops.py
@@ -0,0 +1,11 @@
+# Create a simple "countdown" program that counts down from 10 to 1
+#  using a while loop.
+
+# This program counts down from 10 to 1 using a while loop
+count_down = 10
+
+# Loop until count_down is less than 1
+while count_down >= 1:
+    print(count_down) # Print the current count
+    count_down -= 1 # Decrease the count by 1
+
