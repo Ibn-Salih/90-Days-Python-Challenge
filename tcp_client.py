6ba4bcc9359c56e3f938aeade194266f6386e73b
Ibn-Salih <abdulrahmansuyuti0@gmail.com>
2025-01-20 23:51:04 +0000

    Day 15 updated

--- tcp_client.py
+++ tcp_client.py
@@ -1,10 +1,17 @@
 import socket
 
+# Get the private IP address and port number from the user
 HOST = str(input("Enter private IP address: "))
 PORT = int(input("Enter port number eg'9099': "))
 
+# Create a socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST,PORT))
 
+# Connect to the server
+socket.connect((HOST, PORT))
+
+# Send a message to the server
 socket.send("Hello World!".encode("utf-8"))
+
+# Receive a message from the server and print it
 print(socket.recv(1024))
--- tcp_server.py
+++ tcp_server.py
@@ -5,21 +5,36 @@
 # - Write a simple TCP client-server application using Python’s socket module.
 
 
+
 import socket
 
+# Get the private IP address and port number from the user
 HOST = str(input("Enter private IP address: "))
 PORT = int(input("Enter port number eg'9099': "))
 
+# Create a socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address
 server.bind((HOST, PORT))
 
+# Listen for incoming connections
 server.listen(5)
 
+print("Listening for incoming connections...")
+
 while True:
+    # Accept an incoming connection
     communication_socket, address = server.accept()
     print(f"Connected to {address}")
+
+    # Receive a message from the client
     message = communication_socket.recv(1024).decode("utf8")
+
+    # Send a message back to the client
     communication_socket.send(f"Got your message".encode("utf8"))
+
+    # Close the connection
     communication_socket.close()
     print(f"Connection with {address} ended successfully")
 
